d4602c96f13485d92e798c2297a5e8205d552d42
Andrey <strocksmin@yandex.ru>
2024-02-11 15:00:36 +0300

    refactor: change project structure

---
+++ backend/src/test/java/com/example/crazecode/CrazecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crazecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazecodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
